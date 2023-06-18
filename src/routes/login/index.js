457cf5d8cf1832f487e7838f5c836e07f1c34432
Jawad <jawad.azizi1398@gmail.com>
2023-06-18 21:27:26 +0430

    form action example added

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,38 +1,54 @@
 import {tag} from '../../ui/index.js'
 
 export const actions = {
+    //renders the page
     default: (req, res)=>{
-        console.log('defalt actions callled')
-        req.loggedIn = true
-        return true
+        //get form data
+        const formData = req.formData
+        if(formData.username =='admin' && formData.password =='123'){
+            //passes the data to page
+            req.loggedIn = true
+        }
+        //when the return is true the page is rendered
+        return true 
     },
+
+    //returns a json response 
     register: (req, res)=>{
-        console.log('register action callled')
-        console.log('form data: ', req.formData)
-        res.setStatus(200).json({message: 'ok'})
+        const formData = req.formData
+        if(formData.username =='admin' && formData.password =='123'){
+            res.setStatus(200).json({message: 'ok'})
+        }else{
+            res.setStatus(301).json({message: 'unauthorized'})
+        }
+        
+        //when the return is false the response should be sent as json mannualy
         return false
-
     },
-    load: async (req, res)=>{
-        console.log('load function called in login')
-    }
-
 }
 
 export default function(req){
-    
-    return tag('form', {htmlHead: '<title>login</title>', method: 'post', action: '/login?action=register'}, [
-        req.loggedIn? tag('div', {}, 'logged in'): '',
-        tag('label', { for: 'username' }, 'Username: '),
-        tag('input', { type: 'text', id: 'username', name: 'username' }),
-      
-        tag('br'),
-      
-        tag('label', { for: 'password' }, 'Password: '),
-        tag('input', { type: 'password', id: 'password', name: 'password' }),
-      
-        tag('br'),
-      
-        tag('input', { type: 'submit', value: 'Submit' })
-      ]);
+    if(req.loggedIn){
+        return  tag('div', {}, 
+        [
+            tag('h1', {}, 'wellcome you are logged in'),
+            tag('a', {href: '/login'}, "back")
+        ]
+        )
+    }else{
+        return tag('form', {htmlHead: '<title>login</title>', method: 'post', action: '/login?action=register'}, [
+            
+            tag('label', { for: 'username' }, 'Username: '),
+            tag('input', { type: 'text', id: 'username', name: 'username', placeholder: 'admin' }),
+            
+            tag('br'),
+            
+            tag('label', { for: 'password' }, 'Password: 123'),
+            tag('input', { type: 'password', id: 'password', name: 'password' }),
+            
+            tag('br'),
+            
+            tag('input', { type: 'submit', value: 'Submit' })
+        ]);
+    }
 }
